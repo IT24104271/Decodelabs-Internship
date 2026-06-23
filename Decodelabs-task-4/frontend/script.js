70bc0109c16ee274d39ae7d1e836010c898c39a4
Nikshika Niththiyananthan <it24104271@my.sliit.lk>
2026-06-23 17:50:15 +0530

    Update script.js

--- Decodelabs-task-4/frontend/script.js
+++ Decodelabs-task-4/frontend/script.js
@@ -60,3 +60,64 @@ catch(error){
 }
 
 });
+
+/*=========================
+Dark Mode
+=========================*/
+
+const themeBtn = document.getElementById("theme-btn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        themeBtn.textContent = "☀️";
+    } else {
+        themeBtn.textContent = "🌙";
+    }
+
+});
+
+/*=========================
+Add To Cart
+=========================*/
+
+let cart = [];
+
+const cartBtn = document.querySelector(".cart-btn");
+
+const cartButtons = document.querySelectorAll(".product-card button");
+
+cartButtons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const productCard = button.closest(".product-card");
+
+        const productName = productCard.querySelector("h3").textContent;
+
+        cart.push(productName);
+
+        cartBtn.innerHTML = `🛒 ${cart.length}`;
+
+    });
+
+});
+
+cartBtn.addEventListener("click", () => {
+
+    if(cart.length === 0){
+        alert("Cart is empty!");
+        return;
+    }
+
+    let message = "Items in Cart:\n\n";
+
+    cart.forEach((item, index) => {
+        message += `${index + 1}. ${item}\n`;
+    });
+
+    alert(message);
+
+});
